2dc2616c88769ce8b389c8e111b4e6d05e333702
nano <nickbatmanll@gmail.com>
2024-05-18 19:56:05 +0300

    bulid

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,6 +1,7 @@
 import os
+from pathlib import Path
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
 
@@ -80,3 +81,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ROOT_URLCONF = 'foodgram.urls'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
